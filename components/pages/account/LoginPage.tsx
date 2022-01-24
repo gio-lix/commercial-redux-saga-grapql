ce016dd193f56dd90f7f4b5d367d779c39de10af
Giorgi Mosemgvdlishvili <g_mosemgvdlishvili@yahoo.com>
2022-01-24 17:47:29 +0400

    add

--- components/pages/account/LoginPage.tsx
+++ components/pages/account/LoginPage.tsx
@@ -10,6 +10,7 @@ const LoginPage: FC<ILoginPage> = () => {
      <section className='w-2/6 h-auto flex flex-col  border border-black'>
          <h4 className='text-center font-bold'>LOGIN</h4>
              <div className='flex justify-center my-8 '>
+
                  <Link href="/api/auth/login">
                      <a  className='w-52 h-10 font-bold border border-blue-500 text-blue-500 hover:bg-blue-600 hover:text-white'
                      >Sign In With Google</a>
